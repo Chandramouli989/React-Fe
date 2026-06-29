fe8f11232a0dd6402003019ac62bfc52c180d02b
Chandramouli989 <chandramoulipatle@gmail.com>
2026-06-30 01:07:13 +0530

    second day

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div className="cards">
       <div className="top">
         <img src="https://imgs.search.brave.com/KvkePuTrHTCF2cJEnwFbpPTy26zdZaawwZ5YREkpQKU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNzMv/OTUzLzAwMC9zbWFs/bC9hbWF6b24tcGF5/LWxvZ28tZnJlZS1w/bmcucG5n" alt="" />
-        <button>Save</button>
+        <button><Bookmark /></button>
   
       </div>
       <div className="center">
